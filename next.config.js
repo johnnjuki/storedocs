705ec39faf473cca14134bb6c40dfbdc71ef876e
John Njuki <njukijohn2000@gmail.com>
2023-12-01 22:56:53 +0300

    feat: add doc size limit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActionsBodySizeLimit: '50mb',
+  }
+}
 
 module.exports = nextConfig
