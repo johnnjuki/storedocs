770a19f444c99f41ac46d377c5a573d51460c820
John Njuki <njukijohn2000@gmail.com>
2023-12-01 22:28:07 +0300

    fix: prisma migrate for production database bug

--- prisma/migrations/20231129184126_remove_initial_data_field_in_document_data/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `initialData` on the `DocumentData` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "DocumentData" DROP COLUMN "initialData";
